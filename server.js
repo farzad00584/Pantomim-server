8d177c2ed711fabf5b6d94d21ffc54aac9d866c7
farzad00584 <yaftanyaftan2200584@gmail.com>
2026-06-03 01:05:16 +0330

    Update server.js

--- server.js
+++ server.js
@@ -3,27 +3,59 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*"
+const io = new Server(server,{
+  cors:{
+    origin:"*"
   }
 });
 
-app.get("/", (req, res) => {
+const rooms = {};
+
+app.get("/",(req,res)=>{
   res.send("Server Online");
 });
 
-io.on("connection", (socket) => {
-  console.log("Player Connected:", socket.id);
+io.on("connection",(socket)=>{
 
-  socket.on("disconnect", () => {
-    console.log("Player Disconnected:", socket.id);
+  console.log("Connected:",socket.id);
+
+  socket.on("createRoom",()=>{
+
+    const roomCode =
+      Math.floor(1000 + Math.random()*9000).toString();
+
+    rooms[roomCode] = {
+      players:[socket.id]
+    };
+
+    socket.join(roomCode);
+
+    socket.emit("roomCreated",roomCode);
+
   });
+
+  socket.on("joinRoom",(roomCode)=>{
+
+    if(!rooms[roomCode]){
+      socket.emit("errorMsg","اتاق پیدا نشد");
+      return;
+    }
+
+    if(rooms[roomCode].players.length >= 2){
+      socket.emit("errorMsg","اتاق پر است");
+      return;
+    }
+
+    rooms[roomCode].players.push(socket.id);
+
+    socket.join(roomCode);
+
+    io.to(roomCode).emit("gameStart");
+
+  });
+
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("Server Started");
-});
+server.listen(process.env.PORT || 3000);
